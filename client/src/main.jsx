03a247600eda795a58d5747a2de9fbecbdb104cc
nidhinagpure <nagpurenidhi8@gmail.com>
2025-08-05 18:51:21 +0530

    react-router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from './views/Home'
 
 createRoot(document.getElementById('root')).render(
