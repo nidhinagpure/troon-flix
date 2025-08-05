87e99b3f0d5f4121a96b8c07728504652b813673
nidhinagpure <nagpurenidhi8@gmail.com>
2025-08-05 21:00:32 +0530

    image added in frontend

--- client/src/components/TvShowCard.jsx
+++ client/src/components/TvShowCard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function TvShowCard({_id, title, timing, channel, thumbnail}) {
   return (
     <div>
-      <img src={thumbnail}/>
+      <img src={thumbnail} alt={title} className="h-[250px] w-[400px] object-cover"/>
       <h2>{timing}</h2>
-       <h1>{title}</h1> 
+      <h1>{title}</h1> 
     </div>
   )
 }
