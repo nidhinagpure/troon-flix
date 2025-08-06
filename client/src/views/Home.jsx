bd63ee0a32f5823d6654cf480cfbdd447c1a220a
nidhinagpure <nagpurenidhi8@gmail.com>
2025-08-06 21:01:54 +0530

    set card in array

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -9,8 +9,30 @@ import Button from "../components/Button"
 import { SquarePlus as SquarePlusIcon } from "lucide-react";
 
 
+
+
 function Home() {
   const [tvShows, setTvShows] = useState([]);
+  const fixTvShow = [
+    {
+      title: "Shaka Boom Boom",
+      timing: "5:00pm - 6:00pm",
+      channel: "star utsav",
+      thumbnail: "https://static.toiimg.com/thumb/resizemode-4,width-1280,height-720,msid-49091268/49091268.jpg",
+    },
+    {
+      title: "shaktimaan",
+      timing: "4:00pm - 5:00pm",
+      channel: "star utsav",
+      thumbnail: "https://i.ytimg.com/vi/Ux70bUb4tyo/maxresdefault.jpg",
+    },
+    {
+      title: "Ring wrong ring",
+      timing: "7:00pm - 8:00pm",
+      channel: "star utsav",
+      thumbnail: "https://m.media-amazon.com/images/M/MV5BYWU1YTUxYzItOTY5ZS00ZTQ3LTk1MmMtYjQ5ZTQ0NGEwMDVjXkEyXkFqcGc@._V1_.jpg",
+    },    
+  ]
   const [isModelOpen, setModelOpen] = useState(false);
 
   //for new show
@@ -25,10 +47,10 @@ function Home() {
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/tv_shows`, {
-        title:newShow.title,
-        timing:newShow.timing,
-        channel:newShow.channel,
-        thumbnail:newShow.thumbnail,
+        title: newShow.title,
+        timing: newShow.timing,
+        channel: newShow.channel,
+        thumbnail: newShow.thumbnail,
       }
       );
       toast.success(response.data.message);
@@ -60,11 +82,25 @@ function Home() {
   return (
     <div>
       <h1 className='h-15 w-15 cursor-pointer fixed bottom-5 right-5 bg-blue-500 text-white  shadow-lg hover:bg-blue-600 transition duration-300 flex items-center justify-center rounded-full z-50'
-        onClick={() => { setModelOpen(true);
-         }}>
+        onClick={() => {
+          setModelOpen(true);
+        }}>
         <CirclePlusIcon className='h-14 w-14 cursor-pointer'
         /> </h1>
       <h1 className="text-4xl text-center pt-8 font-bold">Laugh Out Loud on TroonFlix -😍</h1>
+      <div className='flex flex-wrap justify-center pt-6'>
+      {fixTvShow.map((fixObj) => {
+          const {title, timing, channel, thumbnail } = fixObj;
+          return(
+            <TvShowCard
+              title={title}
+              timing={timing}
+              channel={channel}
+              thumbnail={thumbnail}
+            />
+          );
+      })}
+      </div>
 
       <div className='flex flex-wrap justify-center pt-6'>
         {tvShows.map((serialObj) => {
@@ -94,9 +130,9 @@ function Home() {
             type="text"
             placeholder="Show Name"
             value={newShow.title}
-            onChange={(val) =>
-               {setNewShows({ ...newShow, title: val })
-             }}
+            onChange={(val) => {
+              setNewShows({ ...newShow, title: val })
+            }}
             lable=""
             size='md'
           />
@@ -132,17 +168,17 @@ function Home() {
             />
           ) : null}
           <div>
-            
-              <Button
-                title={"Add Show"}
-                onClick={() => {
-                  addTvshow();
-                }}
-                variant='primary'
-                size="lg"
-                 icon={<SquarePlusIcon className="h-5 w-5 inline" />}
+
+            <Button
+              title={"Add Show"}
+              onClick={() => {
+                addTvshow();
+              }}
+              variant='primary'
+              size="lg"
+              icon={<SquarePlusIcon className="h-5 w-5 inline" />}
               iconPosition={"right"}
-              />
+            />
           </div>
 
 
